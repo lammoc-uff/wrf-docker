3633be22f83b70a1b961c79c3a0ecb5d3834f1df
louise-who <louisefonseca@id.uff.br>
2021-09-15 17:52:57 -0300

    consertando

--- scripts/build.sh
+++ scripts/build.sh
@@ -64,5 +64,28 @@ sed -i '39s+glade/p/work/wrfhelp/WPS_GEOG+'$PARENTPATH'/paralelo/GEOG_files/WPS_
 
 # Se tudo deu certo, aparecerão 3 executaveis do geogrid, metgrid e ungrib no diretorio WPS
 
+################# INSTALAÇÃO DO ARWPost ##################
+
+echo "instalando ARWpost..."
+
+cd "$PARENTPATH/paralelo/ARWpost"
+#3 = opção 3 ( gfortran)
+echo "3" | ./configure
+sed -i '38d' configure.arwp
+sed -i '38i CPP             =       /lib/cpp -P -traditional' configure.arwp
+
+# editando Makefile
+
+cd "$PARENTPATH/paralelo/ARWpost/src"
+sed -i '19d' Makefile
+sed -i '19i\                -L$(NETCDF)/lib -I$(NETCDF)/include  -lnetcdff -lnetcdf' Makefile
+
+# Copiamos a pasta include do netcdf para o diretório raiz do WRF e depois compilamos o ARWpost
+
+cd "$PARENTPATH/paralelo/bibliotecas/netcdf-4.1.3"
+cp -r include ../../
+cd "$PARENTPATH/paralelo/ARWpost"
+./compile
+
 
 
